5cf1f4a189f4cf98ddde707c5285adde07f5e85b
kapi1994 <nemanjajovicic13@gmail.com>
2023-04-04 22:30:30 +0200

    Try catch blook is added

--- models/publishers/filter.php
+++ models/publishers/filter.php
@@ -2,13 +2,18 @@
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $limit = $_GET['limit_page'];
 
-    require_once '../../config/connection.php';
-    include '../function.php';
+    try {
+        require_once '../../config/connection.php';
+        include '../function.php';
 
-    echo json_encode([
-        'publishers' => getAllPublishers($limit),
-        'pagination' => pagination('publishers')
-    ]);
+        echo json_encode([
+            'publishers' => getAllPublishers($limit),
+            'pagination' => pagination('publishers')
+        ]);
+    } catch (PDOException $th) {
+        echo json_encode("Something went wrong!");
+        http_response_code(500);
+    }
 } else {
     http_response_code(404);
 }
