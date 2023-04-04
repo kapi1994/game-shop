95d0074468c3ac605ec1e627fea2fe43fdda749e
kapi1994 <nemanjajovicic13@gmail.com>
2023-04-04 23:58:04 +0200

    Db queries replaced with functions

--- models/platforms/update.php
+++ models/platforms/update.php
@@ -19,26 +19,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     } else {
         try {
             require_once '../../config/connection.php';
-
-            $queryCheck = "select id, name from platforms where name ='$name'";
-            $check = $connection->query($queryCheck)->fetch();
+            include '../function.php';
+            $check = checkPlatformName($name);
 
             if ($check && $check->id != $id && $check->name == $name) {
                 echo json_encode("Platform with same name allready exists");
                 http_response_code(409);
             } else {
 
-                $date = date("Y-m-d H:i:s");
-                $queryUpdate = "update platforms set name = ?, updated_at = ? where id = ?";
-                $update = $connection->prepare($queryUpdate);
-                $update->execute([$name, $date, $id]);
-
-                $querySelect = "select * from platforms where id = '$id'";
-                $select = $connection->query($querySelect)->fetch();
-                echo json_encode($select);
+                updatePlatform($name, $id);
+                $platform = getPlatformFullRow($id);
+                echo json_encode($platform);
             }
         } catch (PDOException $th) {
-            echo json_encode($th->getMessage());
+            echo json_encode("Something went wrong!");
             http_response_code(500);
         }
     }
