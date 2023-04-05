c110513ceae8e2ef8ef1a9ff96286582bc49838d
kapi1994 <nemanjajovicic13@gmail.com>
2023-04-05 17:18:43 +0200

    Db query replaced with function

--- models/games/edit.php
+++ models/games/edit.php
@@ -3,12 +3,15 @@ header("Content-type:application/json");
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $id = $_GET['id'];
 
-    require_once '../../config/connection.php';
-    include '../function.php';
-    $querySelect = "select id, name, description, publisher_id from games where id='$id'";
-    $game = $connection->query($querySelect)->fetch();
-    $game->genres = gamePivot($game->id);
-    echo json_encode($game);
+    try {
+        require_once '../../config/connection.php';
+        include '../function.php';
+        $game = getGame($id);
+        echo json_encode($game);
+    } catch (PDOException $th) {
+        echo json_encode("Something went wrong!");
+        http_response_code(500);
+    }
 } else {
     http_response_code(404);
 }
