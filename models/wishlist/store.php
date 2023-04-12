8e089729375b78fdc8a665c463e7f0c2d3c12c04
kapi1994 <nemanjajovicic13@gmail.com>
2023-04-12 18:51:50 +0200

    New response message

--- models/wishlist/store.php
+++ models/wishlist/store.php
@@ -11,7 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $user_id =  $_SESSION['user']->id;
         wishlistStore($user_id, $game_id);
 
-        echo json_encode("New item has been added to your wishlist");
+        echo json_encode("Item has been added to wishlist");
         http_response_code(201);
     } catch (PDOException $th) {
         echo json_encode($th->getMessage());
