32c35a2a921d4998561e3d5796b95e2cb255da9e
kapi1994 <nemanjajovicic13@gmail.com>
2023-04-13 19:40:35 +0200

    Contact page

---
+++ includes/user/contact.php
@@ -0,0 +1,33 @@
+<main class="container">
+    <div class="row mt-5">
+        <div class="col-lg-6 mx-auto">
+            <h1 class="text-center mb-3">Contact us</h1>
+            <div id="contact_response_message"></div>
+            <form action="#" id="contact-form">
+                <div class="mb-3">
+                    <label for="first_name" class="mb-1">First name</label>
+                    <input type="text" name="first_name" id="first_name" class="form-control">
+                    <div id="first_name_error" class="mt-1"></div>
+                </div>
+                <div class="mb-3">
+                    <label for="last_name" class="mb-1">Last name</label>
+                    <input type="text" name="last_name" id="last_name" class="form-control">
+                    <div id="last_name_error" class="mt-1"></div>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="mb-1">Email</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                    <div id="email_error" class="mt-1"></div>
+                </div>
+                <div class="mb-3">
+                    <label for="message" class="mb-1">Message</label>
+                    <textarea name="message" id="message" cols="30" rows="5" class="form-control"></textarea>
+                    <div id="message_error" class="mt-1"></div>
+                </div>
+                <div class="col-lg-3 ms-auto mb-3 mb-lg-0">
+                    <div class="d-grid"><button class="btn btn-primary" type="button" id="btnContactUs">Contact us</button></div>
+                </div>
+            </form>
+        </div>
+    </div>
+</main>
