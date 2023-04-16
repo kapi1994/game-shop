5dfc51dcb8872e2cfc587e2c4e988bd4a531072c
kapi1994 <nemanjajovicic13@gmail.com>
2023-04-16 12:39:37 +0200

    navigation links

--- includes/fixed/navigation.php
+++ includes/fixed/navigation.php
@@ -1,6 +1,16 @@
+<?php
+$userMenuItems = userMenu();
+$adminMenuItems = adminMenu();
+$homeLink = "";
+if (!isset($_SESSION['user']) || isset($_SESSION['user']) && $_SESSION['user']->role_id == 1) {
+    $homeLink = "index.php?page=home";
+} else if (isset($_SESSION['user']) && $_SESSION['user']->role_id == 2) {
+    $homeLink = "admin.php?page=home";
+}
+?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="<?= $homeLink ?>">Navbar</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,14 +18,21 @@
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <?php
                 if (!isset($_SESSION['user']) || isset($_SESSION['user']) && $_SESSION['user']->role_id == 1) :
+                    foreach ($userMenuItems as $userMenuItem) :
+                        $menuName = $userMenuItem->name;
                 ?>
-                    <li class="nav-item"><a href="index.php?page=contact" class="nav-link">Contact</a></li>
-                <?php elseif (isset($_SESSION['user']) && $_SESSION['user']->role_id == 2) : ?>
-                    <li class="nav-item"><a href="admin.php?page=home" class="nav-link <?php if (isset($_GET['page']) && $_GET['page'] == 'home') : ?> active border-bottom fw-bold <?php endif; ?>">Home</a></li>
-                    <li class="nav-item"><a href="admin.php?page=publishers" class="nav-link <?php if (isset($_GET['page']) && $_GET['page'] == 'publishers') : ?> border-bottom fw-bold active <?php endif; ?>">Publishers</a></li>
-                    <li class="nav-item"><a href="admin.php?page=platforms" class="nav-link <?php if (isset($_GET['page']) && $_GET['page'] == 'platforms') : ?> active fw-bold border-bottom <?php endif; ?>">Platforms</a></li>
-                    <li class="nav-item"><a href="admin.php?page=games" class="nav-link <?php if (isset($_GET['page']) && $_GET['page'] == 'games') : ?> fw-bold active border-bottom <?php endif; ?>">Games</a></li>
-                <?php endif; ?>
+                        <li class="nav-item"><a href="index.php?page=<?= strtolower($menuName) ?>" class="nav-link
+                            <?php if (isset($_GET['page']) && $_GET['page'] == strtolower($menuName)) : ?> fw-bold active border-bottom<?php endif; ?>
+                        "><?= $menuName ?></a></li>
+                    <?php
+                    endforeach;
+                elseif (isset($_SESSION['user']) && $_SESSION['user']->role_id == 2) :
+                    foreach ($adminMenuItems as $adminMenuItem) :
+                        $menuName = $adminMenuItem->name; ?>
+                        <li class="nav-item"><a href="admin.php?page=<?= strtolower($menuName) ?>" class="nav-link <?php if (isset($_GET['page']) && $_GET['page'] == strtolower($menuName)) : ?> active border-bottom fw-bold <?php endif; ?>"><?= $menuName ?></a></li>
+
+                <?php endforeach;
+                endif; ?>
             </ul>
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <?php if (!isset($_SESSION['user'])) : ?>
@@ -26,6 +43,11 @@
                 <?php if (isset($_GET['page']) && $_GET['page'] == 'register') : ?> active border-bottom fw-bold <?php endif; ?>
                 ">Register</a></li>
                 <?php else : ?>
+                    <?php
+                    if (isset($_SESSION['user']) && $_SESSION['user']->role_id == 1) : ?>
+                        <li class="nav-item"><a href="index.php?page=cart" class="nav-link <?php if (isset($_GET['page']) && $_GET['page'] == 'cart') : ?> fw-bold active border-bottom <?php endif; ?>">Cart</a></li>
+                        <li class="nav-item"><a href="index.php?page=wishlist" class="nav-link <?php if (isset($_GET['page']) && $_GET['page'] == 'wishlist') : ?> fw-bold active border-bottom <?php endif; ?> ">Wishlist</a></li>
+                    <?php endif; ?>
                     <li class="nav-item"><a href="models/auth/logout.php" class="nav-link">Logout</a></li>
                 <?php endif; ?>
             </ul>
